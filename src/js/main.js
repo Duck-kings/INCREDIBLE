51342c81bdb5a7037c7b82af4f099d2f76e1b865
Duck-kings <sergiykovalenko6661@gmail.com>
2022-07-03 17:43:37 +0300

    Finished with styles for aside and about-section inclusive media query.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import '../assets/styles/main.scss';
+
+const wrapper = document.querySelector('.wrapper');
+const sidebar = document.querySelector('.sidebar');
+const close_btn = document.querySelector('.close__btn');
+const burger_btn = document.querySelector('.burger__menu');
+
+burger_btn.addEventListener('click', () => {
+    sidebar.classList.add('open');
+    wrapper.classList.add('fixed');
+    burger_btn.style.display = 'none';
+});
+
+close_btn.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+    wrapper.classList.remove('fixed');
+    burger_btn.style.display = 'flex';
+});
